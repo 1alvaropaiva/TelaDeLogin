d082723ecbd6c1e2889bd9edfa369abdd86f2c60
1alvaropaiva <alvaropaiva.dev@gmail.com>
2024-08-19 21:46:43 -0300

    add: frames

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+root = Tk ()
 
+class Application():
+    def __init__(self):
+        self.root = root
+        self.tela()
+        self.frames_da_tela()
+        root.mainloop()
+    def tela(self):
+        self.root.title("Alvaro")
+        self.root.configure(background='#1e3743')
+        self.root.geometry("700x500")
+        self.root.resizable(True, True)
+        self.root.maxsize(width=900, height=700)
+        self.root.minsize(width=700, height=500)
+    def frames_da_tela(self):
+        self.frame_1 = Frame(self.root, bd= 4, bg='red', highlightbackground='white', highlightthickness=2)
+        self.frame_1.place(relx= 0.02, rely= 0.02, relwidth= 0.96, relheight=0.46)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        self.frame_2 = Frame(self.root, bd=4, bg='red', highlightbackground='white', highlightthickness=2)
+        self.frame_2.place(relx=0.02, rely=0.52, relwidth=0.96, relheight=0.46)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+Application()
